61650549cb5d576656eae82c707842794bbbebe2
Phila <fila.veselov21092004@gmail.com>
2023-12-14 05:27:29 +1000

    test2

--- src/app/mainpage/main.js
+++ src/app/mainpage/main.js
@@ -7,17 +7,36 @@ function LogoutMenu() {
   };
 
   return (
-        <div className="section__main section">
-    <div className="container">
-      <div className="menu">
-        <nav><a className="nav_main" href="/">237#</a></nav>
-        <nav><a className="nav" href="">О нас</a></nav>
-        <nav><a className="nav" href="">Поиск проекта</a></nav>
-        <nav><a className="nav" onClick={handleLogout}>Выйти</a></nav>
+        <><div className="section__main section">
+      <div className="container">
+        <div className="menu">
+          <nav><a className="nav_main" href="/mainpage">237#</a></nav>
+          <nav><a className="nav" href="/mainproject">Проекты</a></nav>
+          <nav><a className="nav" href="/profile">Профиль</a></nav>
+          <nav><a className="nav" onClick={handleLogout}>Выйти</a></nav>
+        </div>
       </div>
-
-    </div>
-    </div>
+    </div><div className="section__content section">
+        <div className="container">
+          <div className="testblock">
+            <div className="block1">
+              <h1>Lorem ipsum dolor sit</h1>
+              <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h2>
+            </div>
+            <div className="block2">
+              <img src="/images/image1.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div><div className="section__cat section">
+        <div className="container">
+          <div className="card">
+            <div className="cards">1</div>
+            <div className="cards">1</div>
+            <div className="cards">1</div>
+          </div>
+        </div>
+      </div></>
 );
 }
 
